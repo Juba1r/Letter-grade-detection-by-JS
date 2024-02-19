4a25ee5a3909cdf0e0860c2d4978243b48d41d68
Jubair Ibn Khaled <160339037+Pro-jubair@users.noreply.github.com>
2024-02-19 14:36:28 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var num = prompt("Enter Number ");
+
+if(num>=80 && num<=100)
+console.log("A+");
+
+else if(num>=70 && num<=79)
+console.log("A");
+
+else if(num>=60 && num<=69)
+console.log("A-");
+
+else if(num>=40 && num<=59)
+console.log("B");
+
+else 
+console.log("Improve");
